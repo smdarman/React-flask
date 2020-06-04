359cc87d93a6cfb199c40cdc314a341974728b58
salaimandarman <smdar08@gmail.com>
2020-06-04 23:35:08 +1000

    heroku better

--- api.py
+++ api.py
@@ -17,7 +17,9 @@ app = Flask(__name__)
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__,
             static_url_path='',
-            static_folder='client/build')
+            static_folder='')
+
+app = Flask(__name__, static_folder='client/build', static_url_path='/')
 
 @app.route('/')
 def index():
