8129b241956644970c69cd8892ddbdccd53f169c
salaimandarman <smdar08@gmail.com>
2020-06-24 00:41:12 +1000

    changes applied

---
+++ client/build/precache-manifest.a8c102ef02e8173c37c2fcc3b4b6136c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e61edf398a533adb1b570e42425f3ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9a005f99581b4dff54",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d107c8d4bbb2c5d34e1f",
+    "url": "/static/js/2.75e6e932.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.75e6e932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9a005f99581b4dff54",
+    "url": "/static/js/main.d17c84b6.chunk.js"
+  },
+  {
+    "revision": "c298cbb02d5f7788eb6b",
+    "url": "/static/js/runtime-main.f8f1b99c.js"
+  }
+]);
