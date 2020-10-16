d8bef09b6dc97ca7792866d9fcb6fe0eeb400437
salaiman <smdarman@yahoo.com>
2020-10-16 11:58:31 +1100

    my new add mac161158

--- api.py
+++ api.py
@@ -28,7 +28,7 @@ API = os.environ.get('API_KEY')
 #             static_url_path='',
 #             static_folder='client/build')
 
-app = Flask(__name__, static_folder='client/build', static_url_path='/')
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 CORS(app)
 app.config['DEBUG'] = True
 
