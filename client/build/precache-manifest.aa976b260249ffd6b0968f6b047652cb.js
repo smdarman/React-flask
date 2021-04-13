741bdba384bbc786574c28f7d431d07988dba6a4
salaimandarman <smdar08@gmail.com>
2021-04-13 16:17:53 +1000

    macbook 1304c

---
+++ client/build/precache-manifest.aa976b260249ffd6b0968f6b047652cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1795672c1a57142694a75c23fd0a329a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bedf94a1f12d39f62e8b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7271eebf726066e47480",
+    "url": "/static/js/2.316c0f62.chunk.js"
+  },
+  {
+    "revision": "2cf045eda31ce7669329081bc0fd503a",
+    "url": "/static/js/2.316c0f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedf94a1f12d39f62e8b",
+    "url": "/static/js/main.1bf09550.chunk.js"
+  },
+  {
+    "revision": "c298cbb02d5f7788eb6b",
+    "url": "/static/js/runtime-main.f8f1b99c.js"
+  }
+]);
