bb3efec5d8e93e6538467258dae830d90b61337c
salaiman <smdarman@yahoo.com>
2021-07-06 16:59:19 +1000

    Merge branch 'master' of https://github.com/smdarman/React-flask
    # Conflicts:
    #	api.py
    #	client/src/App/App.js
    #	client/src/App/pages/Home.js
    #	client/src/App/pages/News.js
    #	client/src/App/pages/db.js
    #	client/src/App/pages/topic.js
    #	client/src/App/pages/tweet.js

---
+++ client/build/precache-manifest.afcc0646498a51bd4e123f514aff7cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd61b9590fb2015f92a8c6080ad2c0a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a9ba0046cbee3456d2",
+    "url": "/static/css/main.7c4c69d3.chunk.css"
+  },
+  {
+    "revision": "f1b5e9870c194eadc35c",
+    "url": "/static/js/2.047a699e.chunk.js"
+  },
+  {
+    "revision": "2cf045eda31ce7669329081bc0fd503a",
+    "url": "/static/js/2.047a699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a9ba0046cbee3456d2",
+    "url": "/static/js/main.486f9e70.chunk.js"
+  },
+  {
+    "revision": "c298cbb02d5f7788eb6b",
+    "url": "/static/js/runtime-main.f8f1b99c.js"
+  }
+]);
